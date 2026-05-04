6356d121c34f738db535093e92e99ca3de720f71
Angelo Benhanan Abinaya Fuun <angelo.benhanan@ui.ac.id>
2026-05-04 15:45:48 +0700

    Created the class-based views

--- elections/models.py
+++ elections/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-STATUS_ENUM = ["Pending", "Ongoing", "Closed"]
+STATUS_ENUM = ["Pending", "Open", "Closed"]
 
 class Election(models.Model):
     title = models.CharField(max_length=200)
