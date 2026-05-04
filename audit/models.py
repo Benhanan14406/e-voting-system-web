f21616d64f6d004badde876083994e1bf45816de
Angelo Benhanan Abinaya Fuun <angelo.benhanan@ui.ac.id>
2026-05-04 13:25:04 +0700

    Created AuditLog model

--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class AuditLog(models.Model):
+    user= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    action = models.CharField(max_length=100)
+    details = models.TextField(blank=True)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    integrity_hash = models.CharField(max_length=64)
+    logged_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-logged_at"]
