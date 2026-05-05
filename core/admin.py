8e80b09ce895f7fb21fba7e8e2349879b53ae180
Angelo Benhanan Abinaya Fuun <angelo.benhanan@ui.ac.id>
2026-05-05 17:29:10 +0700

    Cleared core's admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,3 @@
-from django.apps import AppConfig
+from django.contrib import admin
 
-class CoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
+# Register your models here.
