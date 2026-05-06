3d1ebf3a735c6531937d568b0683bf55715b300d
Angelo Benhanan Abinaya Fuun <angelo.benhanan@ui.ac.id>
2026-05-06 12:34:09 +0700

    Fixed some bugs and created admin and voter accounts

--- accounts/validators.py
+++ accounts/validators.py
@@ -2,7 +2,7 @@ import re
 from django.core.exceptions import ValidationError
 
 class StrongPasswordValidator:
-    def validate(self, password):
+    def validate(self, password, user=None):
         if not re.search(r'[A-Z]', password):
             raise ValidationError("Password must contain at least one uppercase letter.")
         if not re.search(r'[a-z]', password):
