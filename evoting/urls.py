df874b4f30134ed08816ca77499369118edf823a
Angelo Benhanan Abinaya Fuun <angelo.benhanan@ui.ac.id>
2026-05-06 13:22:06 +0700

    Added django-captcha's URLs

--- evoting/urls.py
+++ evoting/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('accounts.urls')),
     path('', include('audit.urls')),
     path('', include('core.urls')),
